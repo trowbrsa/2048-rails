02403e1931a7505809ed1e80cdbb2876662a3186
Charles Ellis <hamled@hamled.name>
2016-02-08 08:47:47 -0800

    Rename project to 2048-rails

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Twenty48Api
+module Twenty48Rails
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -16,4 +16,4 @@
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-Twenty48Api::Application.config.secret_token = 'f0eb1b2d9b74a6a59544d02106c785c24a0cc0766ed1caa6674bac5c406c5a90dc9f6cc2d3097c9beeedc12ec56f038454ee5559e3b6d747cef6a20265c88069'
+Twenty48Rails::Application.config.secret_token = 'f0eb1b2d9b74a6a59544d02106c785c24a0cc0766ed1caa6674bac5c406c5a90dc9f6cc2d3097c9beeedc12ec56f038454ee5559e3b6d747cef6a20265c88069'
