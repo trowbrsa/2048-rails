4892181b7d8254f48a52268fa6dd9101c4264301
Sarah <trowbrsa@gmail.com>
2016-02-08 15:49:00 -0800

    Got a successful request to Github to log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   root "site#index"
-  get "/auth/github/callback", to: "sessions#create"
+  get "/auth/:provider/callback", to: "sessions#create"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160208233505) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "uid"
+    t.string   "provider"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
